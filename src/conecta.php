06163e468588efbbf5912a13f6da4bfdbcae8765
Marina Tanaka <huttenloucher@gmail.com>
2023-08-24 09:36:14 -0300

    Adicionado try e catch para exemplo

--- src/conecta.php
+++ src/conecta.php
@@ -9,7 +9,20 @@ $banco = "vendas";
 
 /* Configurações de conexão API/Driver utilizando PDO (PHP Data Object)*/
 
-// Instância/Objeto PDO para conexão
-$conexao = new PDO(
-    "mysql:host=$servidor;dbname=$banco;charset=utf8"
-);
+try {
+    // Instância/Objeto PDO para conexão
+    $conexao = new PDO(
+        "mysql:host=$servidor;dbname=$banco;charset=utf8",
+        $usuario, $senha
+    );
+    
+    // Habilitar a verficação e sinalização de erros (exceções)
+    $conexao->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+} catch(Exception $erro){
+    // Exception = Classe/Tipo de dados voltado para tratamento de erros
+    die("Falha na conexão do servidor");
+}
+
