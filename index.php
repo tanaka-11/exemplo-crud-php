3509f3bb4aa47d7dd7b065e905b58046cc9bf046
Marina Tanaka <huttenloucher@gmail.com>
2023-08-24 08:46:46 -0300

    criado primeira e segunda pagina para o exemplo
    Contendo HTML básico

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>CRUD PHP e SQL</title>
+
+</head>
+
+<body>
+    <!-- Utilizando CRUD em PHP e SQL para montar um sistema de vendas -->
+    <h1>---Sistema de Vendas---</h1>
+    <hr>
+    <p>Operações de <b>inserção</b>, <b>leitura</b>, <b>atualização</b>, e <b>exclusão</b> de dados.</p>
+    <hr>
+    <h2>Escolha o que deseja gerenciar:</h2>
+    <ul>
+        <li><a href="./fabricantes/visualizar.php">Fabricantes</a></li>
+        <li><a href="">Produtos</a></li>
+    </ul>
+
+</body>
+
+</html>
