081c275c78c84ef7ae8aadc859c293455c552baa
Marina Tanaka <huttenloucher@gmail.com>
2023-08-24 16:15:07 -0300

    Update css

--- index.php
+++ index.php
@@ -13,20 +13,23 @@
 
 <body>
     <!-- Utilizando CRUD em PHP e SQL para montar um sistema de vendas -->
-    <h1>---Sistema de Vendas---</h1>
-    <hr>
-
-    <p>Operações de <b>inserção</b>, <b>leitura</b>, <b>atualização</b>, e <b>exclusão</b> de dados.</p>
+    <h1>----- Sistema de Vendas -----</h1>
     <hr>
 
     <h2>Escolha o que deseja gerenciar:</h2>
+
+    <div class="text-center">
+    <p>Operações de <b>inserção</b>, <b>leitura</b>, <b>atualização</b>, e <b>exclusão</b> de dados.</p>
+    </div>
+    <hr>
+
+    <div class="button-index">
     <ul>
         <li><a href="./fabricantes/visualizar.php">Fabricantes</a></li>
         <li><a href="">Produtos</a></li>
     </ul>
+    </div>
 
-    <!-- Criando um SCRIPT de Conexão -->
-    
 </body>
 
 </html>
