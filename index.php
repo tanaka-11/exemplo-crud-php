9c6cfd22572298ebc3f091526f07390b320cbc8e
Marina Tanaka <huttenloucher@gmail.com>
2023-08-28 12:00:17 -0300

    Adicionado produtos

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
     
     <ul>
         <li><a href="./fabricantes/visualizar.php">Fabricantes</a></li>
-        <li><a href="">Produtos</a></li>
+        <li><a href="./produtos/visualizar.php">Produtos</a></li>
     </ul>
 
 </div>
---
+++ produtos/visualizar.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Produtos - Visualização</title>
+
+    <link rel="stylesheet" href="../css/exemplo-crud-php.css">
+
+</head>
+<body>
+    <h1>Produtos | SELECT</h1>
+    <hr>
+
+    <div class="center">
+    <p><a href="inserir.php">Cadastre novo produto</a></p>
+    </div>
+
+    <div class="produtos">
+    
+    <article class="produto">
+        <p><b>Nome do Produto:</b>...</p>
+        <p><b>Preço:</b>...</p>
+        <p><b>Quantidade:</b>...</p>   
+    </article>
+
+    <article class="produto">
+        <p><b>Nome do Produto:</b>...</p>
+        <p><b>Preço:</b>...</p>
+        <p><b>Quantidade:</b>...</p>   
+    </article>
+    </div>
+
+    <div class="center">
+    <p><a href="../index.php">Voltar</a></p>
+    </div>
+</body>
+</html>
