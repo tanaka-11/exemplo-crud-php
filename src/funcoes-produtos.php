00d7e51ae31f9a716db4c89cd37b85dca4c053ad
Marina Tanaka <huttenloucher@gmail.com>
2023-08-29 08:37:57 -0300

    Adicionado função de select para produtos

--- src/funcoes-produtos.php
+++ src/funcoes-produtos.php
@@ -2,5 +2,14 @@
 require_once "conecta.php";
 
 function lerProdutos(PDO $conexao):array {
+    $sql = "SELECT nomeProduto, preco, estoque FROM produtos ORDER BY id";
 
+    try {
+        $consulta = $conexao -> prepare($sql);
+        $consulta -> execute();
+        $resultado = $consulta -> fetchAll(PDO::FETCH_ASSOC);
+    } catch (Exception $erro) {
+        die("Erro ao carregar produtos: ".$erro->getMessage());
+    }
+    return $resultado;
 }
