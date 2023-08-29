860d1deb9682781764c1a698ef0882882ce2ee0c
Marina Tanaka <huttenloucher@gmail.com>
2023-08-29 08:20:39 -0300

    retirado div a mais

--- fabricantes/visualizar.php
+++ fabricantes/visualizar.php
@@ -52,7 +52,7 @@ if(isset($_GET["status"]) && $_GET["status"] === "sucesso"){ ?>
     <button id="botao" onclick="esconderMensagem()">Clique aqui para voltar.</button>
     </div>
 <?php } ?>
-</div>
+
 
     <table>
         <caption>
